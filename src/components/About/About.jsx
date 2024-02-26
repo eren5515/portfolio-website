beb59231d836ed9fbd87f49c23cbed523034005e
eren5515 <eren1256@gmail.com>
2024-02-26 16:23:45 +0300

    cdn fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
       
         <div className="about-left-div">
           <img
-            src="https://cdn.discordapp.com/attachments/336171716126572548/1200189836649312327/image.png?ex=65c546e3&is=65b2d1e3&hm=cf65de7c6b97ca5bbd76fe3cf99997532dd971a4219c881a2926fb59d3422ea4&"
+            src="https://raw.githubusercontent.com/eren5515/asd/main/about-img.62b47e7f183d4b4e9feb.webp"
             alt=""
           />
         </div>
